b26a0770efb9a7edbc9ef185f440bcba785e55b9
AliNazariii <ali4nazari4@gmail.com>
2021-12-24 16:59:04 +0330

    fix(comment): fix comment dialog close

--- app/src/main/java/com/example/chicko/ui/CommentDialogFragment.kt
+++ app/src/main/java/com/example/chicko/ui/CommentDialogFragment.kt
@@ -5,29 +5,25 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
 import androidx.fragment.app.DialogFragment
 import com.example.chicko.R
 import com.example.chicko.data.ProvidersDataSource
-import com.example.chicko.databinding.AddCommendDialogBinding
-import java.util.logging.Level.INFO
 
 class CommentDialogFragment : DialogFragment() {
-//    private lateinit var binding: AddCommendDialogBinding
     private lateinit var comment: String
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
 //        getDialog()!!.getWindow()?.setBackgroundDrawableResource(R.drawable.round_corner);
-        super.onCreateView(inflater, container, savedInstanceState)
-
-        val view: View = inflater.inflate(R.layout.add_commend_dialog, container, false)
-        val binding: AddCommendDialogBinding = AddCommendDialogBinding.inflate(layoutInflater)
-
-        binding.submitComment.setOnClickListener{
+        val view: View = inflater.inflate(R.layout.add_comment_dialog, container, false)
+        view.findViewById<Button>(R.id.submit_comment).setOnClickListener {
             Log.w("t", "dddddddddddddddddddddddddddddddddddd")
-            comment = binding.addCommentInput.text.toString()
+            comment = view.findViewById<EditText>(R.id.add_comment_input).text.toString()
             ProvidersDataSource.addComment("shalgham", 1, comment)
             Log.w("Comment", comment)
             activity?.onBackPressed();
@@ -41,6 +37,9 @@ class CommentDialogFragment : DialogFragment() {
         super.onStart()
 //        val width = (resources.displayMetrics.widthPixels * 0.85).toInt()
 //        val height = (resources.displayMetrics.heightPixels * 0.40).toInt()
-        dialog!!.window?.setLayout(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+        dialog!!.window?.setLayout(
+            ViewGroup.LayoutParams.WRAP_CONTENT,
+            ViewGroup.LayoutParams.WRAP_CONTENT
+        )
     }
 }
