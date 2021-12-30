82bed5c0bac2bd0f78f82c845d950b5f8464a4d0
AliNazariii <ali4nazari4@gmail.com>
2021-12-30 22:24:25 +0330

    feat: add search

--- app/src/main/java/com/example/chicko/ui/HomeFragment.kt
+++ app/src/main/java/com/example/chicko/ui/HomeFragment.kt
@@ -1,11 +1,21 @@
 package com.example.chicko.ui
 
+import android.R
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.SearchView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import com.example.chicko.activities.ServiceActivity
+import com.example.chicko.data.Database
 import com.example.chicko.databinding.FragmentHomeBinding
+import com.google.android.material.textview.MaterialTextView
+import com.example.chicko.R as ChickoR
+
 
 class HomeFragment : Fragment() {
 
@@ -25,6 +35,53 @@ class HomeFragment : Fragment() {
         val root: View = binding.root
 
         return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val searchListView = binding.searchListView
+        val searchView = binding.searchView
+        val list = Database.Services.map { it.name }
+
+        searchListView.setOnItemClickListener { parent, view, position, id ->
+            val intent = Intent(context, ServiceActivity::class.java)
+            intent.putExtra(
+                ServiceActivity.SERVICE_ID,
+                Database.Services.find { it.name == (view as MaterialTextView).text.toString() }?.ID
+            )
+            view.context.startActivity(intent)
+        }
+
+        val adapter: ArrayAdapter<String> =
+            ArrayAdapter(view.context, R.layout.simple_list_item_1, list)
+
+        searchListView.adapter = adapter
+        searchListView.visibility = View.GONE
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String): Boolean {
+                if (query.trim().isEmpty()) {
+                    searchListView.visibility = View.GONE
+                } else {
+                    searchListView.visibility = View.VISIBLE
+                }
+
+                if (list.contains(query)) {
+                    adapter.filter.filter(query)
+                } else {
+                    Toast.makeText(context, ChickoR.string.no_match, Toast.LENGTH_LONG).show()
+                }
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String): Boolean {
+                adapter.filter.filter(newText)
+                if (newText.trim().isEmpty()) {
+                    searchListView.visibility = View.GONE
+                } else {
+                    searchListView.visibility = View.VISIBLE
+                }
+                return false
+            }
+        })
     }
 
     override fun onDestroyView() {
